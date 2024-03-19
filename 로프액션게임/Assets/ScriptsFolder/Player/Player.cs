f1dc1080357df182b8bd2abfd62713f8bb93661c
WOO JIN KIM <kwj00310@gmail.com>
2024-03-19 23:55:45 +0900

    Character Move Update_2

--- 로프액션게임/Assets/ScriptsFolder/Player/Player.cs
+++ 로프액션게임/Assets/ScriptsFolder/Player/Player.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class Player : MonoBehaviour
@@ -9,6 +10,7 @@ public class Player : MonoBehaviour
     //이동관련 변수
     float Speed;
     float JumpPower = 4;
+    int JumpCounter=0;
     //참조컴포넌트 관련
     Rigidbody2D rb;
     Animator animator;
@@ -16,7 +18,11 @@ public class Player : MonoBehaviour
     bool WalkFlag;
     bool turnFlag;
     bool RunFlag;
+    bool JumpFlag;
+    bool DownFlag;
     bool jumpRequest = false;
+    public float StopSpeed = 0;
+    float lastSpeed;
     //벡터 변수
     Vector2 movement;
     Vector2 PlayerSize;
@@ -27,7 +33,6 @@ public class Player : MonoBehaviour
         rb.constraints = RigidbodyConstraints2D.FreezeRotation;
         PlayerSize=GetComponent<SpriteRenderer>().bounds.size;
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -35,43 +40,81 @@ public class Player : MonoBehaviour
         movement.y = rb.velocity.y;
         WalkFlag = false;
         RunFlag = false;
-        Speed =Input.GetKey(KeyCode.LeftShift) ? 5 : 2.5f;
-        if (Input.GetKey(KeyCode.LeftShift)) RunFlag = true;
-        if(Input.GetKey(KeyCode.D))
+
+        float currentSpeed = Mathf.Abs(rb.velocity.x);
+        
+ 
+        GetComponent<SpriteRenderer>().flipX = turnFlag;
+        IsDown();
+        CheckMoving();
+        CheckAnimator();
+    }
+    void Jump()
+    {
+        JumpCounter++;
+        rb.AddForce(Vector2.up * JumpPower, ForceMode2D.Impulse);
+        JumpFlag = true;
+            
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        string layerName=LayerMask.LayerToName(collision.gameObject.layer);
+        if (layerName=="Platform")
+        {
+            JumpFlag = false;
+            DownFlag = false;
+            JumpCounter = 0;
+            if(collision.relativeVelocity.y>=6f&& collision.relativeVelocity.y <= 15f)
+            {
+                animator.Play("Down-Stand");
+            }
+            if (collision.relativeVelocity.y > 15)
+            {
+                animator.Play("HardDown-Stand");
+            }
+        }
+    }
+    public void SetLanded()
+    {
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        animator.SetTrigger("Is Landed");
+    }
+    void StopPosition()
+    {
+        rb.constraints = RigidbodyConstraints2D.FreezeAll;
+    }
+    void IsDown()
+    {
+        if(rb.velocity.y<-4f) 
+        {
+            JumpFlag = false;
+            DownFlag = true;
+        }
+    }
+    void CheckMoving()
+    {
+        if (Input.GetKey(KeyCode.D))
         {
             turnFlag = false;
             movement.x = 1;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            turnFlag=true;
+            turnFlag = true;
             movement.x = -1;
         }
-        if (Mathf.Abs(movement.x)==1) WalkFlag = true;
-        GetComponent<SpriteRenderer>().flipX = turnFlag;
-        if (Input.GetKeyDown(KeyCode.F))jumpRequest = true;
-        //애니메이션 설정
-        animator.SetBool("Is Run",RunFlag);
-        animator.SetBool("Is walk", WalkFlag);
-        if (rb.velocity.y < -2.9f)
-        {
-            Debug.DrawRay(this.transform.position, Vector2.down * PlayerSize.y, new Color(0, 1, 0));
-            RaycastHit2D ray = Physics2D.Raycast(this.transform.position, Vector2.down, PlayerSize.y / 2, LayerMask.GetMask("Platform"));
-            animator.SetBool("Is Jump", false);
-            animator.SetBool("Is Down", true);
-            if (ray.collider != null)
-            {
-                if (ray.distance < (PlayerSize.y / 2) - 0.01f)
-                {
-                    animator.SetBool("Is Down", false);
-                }
-            }
-        }
+        Speed = Input.GetKey(KeyCode.LeftShift) ? 5 : 2.5f;
+        
     }
-    void Jump()
+    void CheckAnimator()
     {
-        rb.AddForce(Vector2.up *JumpPower, ForceMode2D.Impulse);
-        animator.SetBool("Is Jump", true);
+        if (Input.GetKey(KeyCode.LeftShift)) RunFlag = true;
+        if (Mathf.Abs(movement.x) == 1) WalkFlag = true;
+        if (Input.GetKeyDown(KeyCode.F)&&JumpCounter < 2) jumpRequest = true;
+        animator.SetBool("Is Run", RunFlag);
+        animator.SetBool("Is walk", WalkFlag);
+        animator.SetBool("Is Jump", JumpFlag);
+        animator.SetBool("Is Down", DownFlag);
     }
     private void FixedUpdate()
 
